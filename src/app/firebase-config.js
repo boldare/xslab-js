75d05f27a10e04d54efe5f34f1ffdd9cb2cdf7ee
Krzysztof Sopa <krzysztof.sopa@xsolve.pl>
2016-11-25 12:36:57 +0100

    TASK-1-B-DONE: Firebase implementation

--- src/app/firebase-config.js
+++ src/app/firebase-config.js
@@ -1,6 +1,7 @@
+//Step-2-B: Adding firebase synchronization - database configuration
 module.exports = {
   apiKey: '<your-api-key>',
   authDomain: '<your-auth-domain>',
-  databaseURL: '<your-database-url>',
+  databaseURL: 'https://xslab-js-68b53.firebaseio.com/',
   storageBucket: '<your-storage-bucket>'
 };
--- src/game/services/games-repository.js
+++ src/game/services/games-repository.js
@@ -6,27 +6,30 @@ var createBoard = function (name) {
   }
 };
 
-module.exports = function($timeout) {
-  //Step-2-A: Mock implementation
-  var roomList = [];
+module.exports = function($q, $timeout, $firebaseArray, firebase, GameShipsGenerator) {
+  //Step-2-B: Adding firebase synchronization
+  var roomListRef = firebase.database().ref().child("roomList");
+  var roomList = $firebaseArray(roomListRef);
 
   return {
     getGames: function () {
       return roomList;
     },
 
-    //Step-2-A: Mock implementation - implement create game method
+    //Step-2-B: Adding firebase synchronization
     createGame: function(gameData)  {
-      return $timeout(function () {
-        return roomList.push(createBoard(gameData.name));
-      }, 1);
+      return roomList.$add(createBoard(gameData.name, GameShipsGenerator)).then(function (ref) {
+        return ref.key;
+      });
     },
 
-    //Step-2-A: Mock implementation - implement get game method
+    //Step-2-B: Adding firebase synchronization
     getGame: function (id) {
-      return $timeout(function () {
-        return roomList[id];
-      }, 1);
+      return roomList
+        .$loaded()
+        .then(function(roomList) {
+          return roomList.$getRecord(id);
+        })
     }
   };
 };
